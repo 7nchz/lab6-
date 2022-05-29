ae52c772b3a4ec6b6e16485452c392c6188ef668
7nchz <101666746+7nchz@users.noreply.github.com>
2022-05-29 22:29:01 +0300

    Add files via upload

---
+++ lab6/src/lab6.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class lab6{
+
+    public static void main(String[] args) {
+
+        ArrayList<String> people = new ArrayList<String>();
+
+        people.add("Tom");
+        people.add("Alice");
+        people.add("Kate");
+        people.add("Sam");
+        people.add(1, "Bob");
+
+        System.out.println(people.get(1));
+        people.set(1, "Robert");
+
+        System.out.printf("ArrayList has %d elements \n", people.size());
+        for(String person : people){
+
+            System.out.println(person);
+        }
+
+        if(people.contains("Tom")){
+
+            System.out.println("ArrayList contains Tom");
+        }
+
+
+        people.remove("Robert");
+
+        people.remove(0);
+
+        Object[] peopleArray = people.toArray();
+        for(Object person : peopleArray){
+
+            System.out.println(person);
+        }
+    }
+}
